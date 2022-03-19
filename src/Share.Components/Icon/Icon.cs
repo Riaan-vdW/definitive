fff73d9de5e49780f61b360c2547e3b756bec50f
Riaan van der Westhuizen <riaan.web.za@outlook.com>
2022-03-19 16:48:38 +0200

    Adding Tailwind CSS

---
+++ src/Share.Components/Icon/Icon.cs
@@ -0,0 +1,13 @@
+﻿namespace Share;
+
+/// <summary>
+/// Icon
+/// </summary>
+public abstract class Icon : ComponentBase
+{
+    /// <summary>
+    /// Class
+    /// </summary>
+    [Parameter]
+    public string Class { get; set; } = default!;
+}
